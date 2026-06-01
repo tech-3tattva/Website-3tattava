c83a2ac4cfdda09a95dc8df09e88680b99ef6a50
tech-3tattva <it@3tattava.com>
2026-06-01 11:59:54 +0530

    maintenance: blank site - offline

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,31 +1,7 @@
-import HeroSection from "@/components/home/HeroSection";
-import TrustStrip from "@/components/home/TrustStrip";
-import ProductHeroSection from "@/components/home/ProductHeroSection";
-import FeaturedProductSpotlight from "@/components/home/FeaturedProductSpotlight";
-import FeaturesSection from "@/components/home/FeaturesSection";
-import WeekByWeek from "@/components/home/WeekByWeek";
-import TestimonialsMarquee from "@/components/home/TestimonialsMarquee";
-import FounderSection from "@/components/home/FounderSection";
-import EducationPreview from "@/components/home/EducationPreview";
-import { WebsiteSchema } from "@/components/seo/JsonLd";
-import { getFeaturedProducts } from "@/lib/products";
+export default function Home() {
+  return null
+}
 
-export default async function HomePage() {
-  const featuredProducts = await getFeaturedProducts();
-  const spotlight = featuredProducts[0];
-
-  return (
-    <>
-      <WebsiteSchema />
-      <HeroSection />
-      <TrustStrip />
-      <ProductHeroSection />
-      {spotlight && <FeaturedProductSpotlight product={spotlight} />}
-      <FeaturesSection />
-      <WeekByWeek />
-      <TestimonialsMarquee />
-      <FounderSection />
-      <EducationPreview />
-    </>
-  );
+export const metadata = {
+  robots: 'noindex, nofollow',
 }
